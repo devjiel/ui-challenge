befee77032f93b301d7977e41ec5fad0db520708
devjiel <jeremy.leconardel@gmail.com>
2025-05-04 23:56:54 +0200

    resize avatar size

--- astro-calendar/src/components/ui/avatar_component.jsx
+++ astro-calendar/src/components/ui/avatar_component.jsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
 export const AvatarComponent = ({ src, alt, fallbackText }) => {
   return (
-    <Avatar>
+    <Avatar className="w-10 h-10">
       <AvatarImage src={src} alt={alt} />
       <AvatarFallback>{fallbackText}</AvatarFallback>
     </Avatar>
