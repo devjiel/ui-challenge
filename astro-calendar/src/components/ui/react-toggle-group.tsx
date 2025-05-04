2d72b14fd1b26202a485d713aa03f4009469417d
devjiel <jeremy.leconardel@gmail.com>
2025-05-04 23:21:52 +0200

    try style toggle

--- astro-calendar/src/components/ui/react-toggle-group.tsx
+++ astro-calendar/src/components/ui/react-toggle-group.tsx
@@ -1,21 +1,26 @@
 import { ToggleGroup, ToggleGroupItem } from './toggle-group';
-
+import { useState } from 'react';
 
 export function ReactToggleGroup() {
+  const [value, setValue] = useState("week");
 
   return (
-    <div className="flex flex-row gap-2">
-    <ToggleGroup type="single" size="sm">
-        <ToggleGroupItem value="bold" aria-label="Toggle bold">
-            <div className="text-sm">Month</div>
-        </ToggleGroupItem>
-        <ToggleGroupItem value="italic" aria-label="Toggle italic">
-            <div className="text-sm">Week</div>
-        </ToggleGroupItem>
-        <ToggleGroupItem value="strikethrough" aria-label="Toggle strikethrough">
-            <div className="text-sm">Day</div>
-        </ToggleGroupItem>
-    </ToggleGroup>
-</div>
+    <div className="flex flex-row gap-2 bg-gray-100 rounded-xl p-2">
+        <ToggleGroup 
+          type="single" 
+          value={value} 
+          onValueChange={(val) => val && setValue(val)}
+        >
+            <ToggleGroupItem value="month" aria-label="Toggle month" className="data-[state=on]:bg-white data-[state=on]:shadow-none data-[state=off]:bg-gray-100 hover:bg-gray-200 rounded-l-md px-4 py-2 font-medium">
+                <div className={`${value === "month" ? "font-bold" : "font-normal"} text-base px-4`}>Month</div>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="week" aria-label="Toggle week" className="data-[state=on]:bg-white data-[state=on]:shadow-none data-[state=off]:bg-gray-100 hover:bg-gray-200 px-4 py-2 font-medium">
+                <div className={`${value === "week" ? "font-bold" : "font-normal"} text-base px-4`}>Week</div>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="day" aria-label="Toggle day" className="data-[state=on]:bg-white data-[state=on]:shadow-none data-[state=off]:bg-gray-100 hover:bg-gray-200 rounded-r-md px-4 py-2 font-medium">
+                <div className={`${value === "day" ? "font-bold" : "font-normal"} text-base px-4`}>Day</div>
+            </ToggleGroupItem>
+        </ToggleGroup>
+    </div>
   );
 } 
