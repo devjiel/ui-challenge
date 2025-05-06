5bacf195757cab82515afa58d6027a72aa8efbc4
jeremy_leconardel <jeremy_leconardel@connect-tech.sncf>
2025-05-06 11:59:41 +0200

    [WIP] Add event in calendar

--- astro-calendar/tailwind.config.js
+++ astro-calendar/tailwind.config.js
@@ -1,8 +1,30 @@
+import { createGlobPatternsForDependencies } from '@nxtensions/astro/tailwind';
+import { join } from 'path';
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    join(
+      __dirname,
+      'src/**/!(*.stories|*.spec).{astro,html,js,jsx,md,svelte,ts,tsx,vue}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        foo: '#123',
+        many: {
+          nested: {
+            objects: {
+              here: '#456',
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [],
-} 
+};
