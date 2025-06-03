4ea9548181e1fae6c44d10d1993b33a34e668128
devjiel <jeremy.leconardel@gmail.com>
2025-06-03 23:14:02 +0200

    Add tabs header

--- dynamic-dashboard/src/app/page.tsx
+++ dynamic-dashboard/src/app/page.tsx
@@ -1,18 +1,32 @@
 import Header from "@/components/layout/Header";
 import ViewCard from "@/components/ui/ViewCard";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
-      <main className="p-6">
+      <main className="p-6 flex flex-col gap-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           <ViewCard title="Total Page Views" />
           <ViewCard title="Active Sessions" />
           <ViewCard title="Unique Users" />
           <ViewCard title="Avg. Session Duration" />
         </div>
+        <Tabs defaultValue="overview">
+          <TabsList>
+            <TabsTrigger value="overview">Overview</TabsTrigger>
+            <TabsTrigger value="page-views">Page Views</TabsTrigger>
+            <TabsTrigger value="sessions">Sessions</TabsTrigger>
+            <TabsTrigger value="interactions">Interactions</TabsTrigger>
+          </TabsList>
+        </Tabs>
       </main>
     </div>
   );
